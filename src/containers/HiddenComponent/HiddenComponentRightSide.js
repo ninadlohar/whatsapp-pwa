b672232436e0cc94a73511ba1db24f3058d438fd
ninadlohar <ninad.lohar94@gmail.com>
2019-05-09 11:37:56 +0530

    fixed the alignment issue, no more alingment bugs

--- src/containers/HiddenComponent/HiddenComponentRightSide.js
+++ src/containers/HiddenComponent/HiddenComponentRightSide.js
@@ -23,13 +23,14 @@ class HiddenComponentRightSide extends React.Component {
     this.setState({ isEditingDescription: !this.state.isEditingDescription });
   };
 
+  /** if user is resizing this fn() is executed when squeezeBoolean(the hidden right component is active) */
   resize = page => {
     if (page > 768 && page <= 900 && this.state.squeezeLayoutBoolean === false) {
-      document.getElementById("col-xl-6-5").style.width = "60%";
-      document.getElementById("DOMChangedHiddenSide").style.width = "0%";
+      document.getElementById("col-xl-6-5").style.width = "0";
+      document.getElementById("DOMChangedHiddenSide").style.width = "60%";
     } else if (page >= 901 && page <= 1024 && this.state.squeezeLayoutBoolean === false) {
-      document.getElementById("col-xl-6-5").style.width = "65%";
-      document.getElementById("DOMChangedHiddenSide").style.width = "0%";
+      document.getElementById("col-xl-6-5").style.width = "0%";
+      document.getElementById("DOMChangedHiddenSide").style.width = "65%";
     } else if (page >= 1025 && page <= 1300 && this.state.squeezeLayoutBoolean === false) {
       document.getElementById("col-xl-6-5").style.width = "65%";
       document.getElementById("DOMChangedHiddenSide").style.width = "0%";
@@ -38,14 +39,14 @@ class HiddenComponentRightSide extends React.Component {
       document.getElementById("DOMChangedHiddenSide").style.width = "0%";
     }
   };
-
+  /** if user is not this fn() is executed */
   withoutResize = page => {
     if (page > 768 && page <= 900 && this.state.squeezeLayoutBoolean === false) {
-      document.getElementById("col-xl-6-5").style.width = "60%";
-      document.getElementById("DOMChangedHiddenSide").style.width = "0%";
+      document.getElementById("col-xl-6-5").style.width = "0%";
+      document.getElementById("DOMChangedHiddenSide").style.width = "60%";
     } else if (page >= 901 && page <= 1024 && this.state.squeezeLayoutBoolean === false) {
-      document.getElementById("col-xl-6-5").style.width = "65%";
-      document.getElementById("DOMChangedHiddenSide").style.width = "0%";
+      document.getElementById("col-xl-6-5").style.width = "0%";
+      document.getElementById("DOMChangedHiddenSide").style.width = "65%";
     } else if (page >= 1025 && page <= 1300 && this.state.squeezeLayoutBoolean === false) {
       document.getElementById("col-xl-6-5").style.width = "65%";
       document.getElementById("DOMChangedHiddenSide").style.width = "0%";
@@ -55,6 +56,7 @@ class HiddenComponentRightSide extends React.Component {
     }
   };
 
+  /** closes the right side drawer */
   closeDrawerFn = page => {
     if (page > 768 && page <= 900 && this.state.squeezeLayoutBoolean === true) {
       document.getElementById("col-xl-6-5").style.width = "60%";
@@ -66,12 +68,12 @@ class HiddenComponentRightSide extends React.Component {
       document.getElementById("col-xl-6-5").style.width = "65%";
       document.getElementById("DOMChangedHiddenSide").style.width = "0%";
     } else if (page >= 1301 && this.state.squeezeLayoutBoolean === true) {
-      console.log(page);
       document.getElementById("col-xl-6-5").style.width = "70%";
       document.getElementById("DOMChangedHiddenSide").style.width = "0%";
     }
   };
 
+  /** here page is the window width */
   slideInToggler = () => {
     let leftSide = document.getElementById("col-xl-3-5");
     let rightSide = document.getElementById("col-xl-6-5");
@@ -92,7 +94,6 @@ class HiddenComponentRightSide extends React.Component {
     let hiddenRightC = document.getElementById("DOMChangedHiddenSide");
     let page = document.getElementById("page");
     this.props.closeDrawer();
-
     this.setState({ squeezeLayoutBoolean: false, clientWidth: page }, () => {
       leftSide.classList.remove("DOMChangedLeftSide", "DOMChangedLeftSide_P-1301");
       rightSide.classList.remove("DOMChangedRightSide", "DOMChangedRightSide_P-1301");
