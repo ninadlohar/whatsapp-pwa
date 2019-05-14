805e7538fff70447fda37706570d4ec1aee0f1fb
ninadlohar <ninad.lohar94@gmail.com>
2019-05-14 17:32:34 +0530

    added social icons

--- src/containers/LeftSide/DropUps/SmallDrawers/InviteFriends.js
+++ src/containers/LeftSide/DropUps/SmallDrawers/InviteFriends.js
@@ -6,28 +6,28 @@ class InviteFriends extends React.Component {
     return (
       <div className="col-12 p-2 px-0 justify-content-around d-flex">
         <div className="row mx-0">
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://linkedin.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://facebook.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://twitter.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://instagram.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://skype.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://whatsapp.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://google.com/in/" />
           </div>
-          <div className="col-3 p-1">
+          <div className="col-3 p-1" onClick={this.props.closeDrawer}>
             <SocialIcon url="http://github.com/in/" />
           </div>
         </div>
